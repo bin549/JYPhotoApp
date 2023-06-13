914c5fd6d65093a22b0602ddc3e02b2afc54bbf9
bin549 <549288160@qq.com>
2023-06-13 13:47:41 +0800

    commit

--- JYVideoApp/ViewController.swift
+++ JYVideoApp/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  JYVideoApp
-//
-//  Created by App on 2023/6/13.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
